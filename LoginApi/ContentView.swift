3d05f5b284eb156587afece400a6e96b2e05b76b
Aji Suryawan <kazumovie132@gmail.com>
2020-08-01 13:44:52 +0700

    Initial Commit

---
+++ LoginApi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoginApi
+//
+//  Created by Aji Suryawan on 01/08/20.
+//  Copyright © 2020 RPL RUS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
